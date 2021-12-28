b63a79872e5555ff70e5fdd22afa2ed0055b0e5d
Nicolas <nicolascoudenne@gmail.com>
2021-12-28 17:54:01 +0100

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+import _ from 'lodash';
+
+
+import '../css/app.scss';
+
+import img from '../img/profil.jpg';
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        'cv': './assets/js/app.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, './public/build'),
+        publicPath: 'build/',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(scss)$/,
+                use: [{
+                    loader: 'style-loader', // inject css to page
+                }, {
+                    loader: 'css-loader'
+                }, {
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            plugins: function() {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    }
+                }, {
+                    loader: 'sass-loader'
+                }]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'static/assets/',
+                    publicPath: 'static/assets/',
+                },
+            },
+        ]
+    },
+    watchOptions: {
+        ignored: ['./node_modules/']
+    },
+    mode: "development"
+};
